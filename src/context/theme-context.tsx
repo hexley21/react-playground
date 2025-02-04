cc20002cf3cbf82b78945189df825802482d65cf
hexley21 <a.kupreishvili.21@gmail.com>
2025-02-04 17:31:56 +0400

    feat(dark-theme): add state provider

---
+++ src/context/theme-context.tsx
@@ -0,0 +1,47 @@
+import { createContext, useState, useEffect, type ReactNode } from 'react';
+import type { ThemeRepository } from '~/domain/theme';
+
+interface ThemeContextType {
+    darkMode: boolean;
+    toggleDarkMode: () => void;
+    repo?: ThemeRepository;
+}
+
+export const ThemeContext = createContext<ThemeContextType>({
+    darkMode: false,
+    toggleDarkMode: () => { },
+});
+
+function safeGetTheme(repo: ThemeRepository) {
+    if (typeof window !== "undefined") {
+      return repo.Get();
+    }
+    return false;
+  }
+  
+  export function ThemeProvider({ children, repo }: { children: React.ReactNode; repo: ThemeRepository }) {
+    const [darkMode, setDarkMode] = useState<boolean>(() => safeGetTheme(repo));
+    
+    const toggleDarkMode = () => {
+      if (typeof window !== "undefined") {
+        repo.Update(!darkMode);
+        setDarkMode(!darkMode);
+      }
+    };
+  
+    useEffect(() => {
+      if (typeof window !== "undefined") {
+        if (darkMode) {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      }
+    }, [darkMode]);
+  
+    return (
+      <ThemeContext.Provider value={{ darkMode, toggleDarkMode, repo }}>
+        {children}
+      </ThemeContext.Provider>
+    );
+  }
