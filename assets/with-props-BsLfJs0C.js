a006cabea6aba89642b2818064a3f52e7a808278
hexley21 <a.kupreishvili.21@gmail.com>
2025-02-05 15:56:07 +0400

    Updates

---
+++ assets/with-props-BsLfJs0C.js
@@ -0,0 +1,1 @@
+import{v as p,w as t,x as e,t as o,r as s,y as u}from"./chunk-IR6S3I6Y-Dtf6j6yn.js";function i(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
